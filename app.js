b1c3cd94a55a08f6b01a9b89a3cccea5471d581e
narekhovhannisyan <narekgilmour@gmail.com>
2017-09-04 18:50:44 +0400

    added configRequest function

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
 const express = require('express')
 const app = express()
+const config = require('./config/config.json')
 
-app.get('/', function(req, res) {
-  res.send('Node worker is running!')
-})
+const configRequest = () => {
+  app.get(config.url, function(req, res) {
+    res.send('done config request!')
+  })
+}
 
 app.post(
   '/init',
   function(req, res, next) {
     res.sendStatus(204)
-    next()
-  },
-  function(req, res) {
-    // TODO: must change worker's state
+    configRequest()
   }
 );
 
+
 app.listen(3000)
-console.log('now worker is running!')
+console.log('now worker is alive!')
