859f2a5e9c4c0b1e2bcdf10a3199ca30cdaf7d43
narekhovhannisyan <narekgilmour@gmail.com>
2017-09-05 18:24:44 +0400

    after posting makes request

--- app.js
+++ app.js
@@ -1,21 +1,27 @@
 const express = require('express')
 const app = express()
 const config = require('./config/config.json')
+const request = require('request-promise')
 
-const configRequest = () => {
-  app.get(config.url, function(req, res) {
-    res.send('done config request!')
-  })
+const makeRequest = () => {
+  const options = {
+    method: 'put',
+    uri: 'http://192.168.2.57:9010/api/v1/workers/1/state',
+    headers: {
+      'User-Agent': 'nodeWorker'
+    },
+    json: true
+  }
+  return request(options)
 }
 
 app.post(
-  '/init',
-  function(req, res, next) {
+  '/api/v1/workers/:id/boot',
+  function (req, res, next) {
     res.sendStatus(204)
-    configRequest()
+    makeRequest()
   }
-);
-
+)
 
 app.listen(3000)
-console.log('now worker is alive!')
+console.log('now worker is booting!')
